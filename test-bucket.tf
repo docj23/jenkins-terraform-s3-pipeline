29e1532d10a815ba794ff0863ba7e13bcdd75c84
docj23 <lshelton32@yahoo.com>
2026-03-28 15:54:47 -0500

    updated bucket to public config

--- test-bucket.tf
+++ test-bucket.tf
@@ -2,28 +2,52 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0" # Use latest version if possible
+      version = "~> 5.0"
     }
   }
 
   backend "s3" {
-    bucket  = "cigarhubsec"                 # Name of the S3 bucket
-    key     = "jenkins-test-031726.tfstate"        # The name of the state file in the bucket
-    region  = "us-east-2"                          # Use a variable for the region
-    encrypt = true                                 # Enable server-side encryption (optional but recommended)
-  } 
+    bucket  = "cigarhubsec"
+    key     = "jenkins-test-031726.tfstate"
+    region  = "us-east-2"
+    encrypt = true
+  }
 }
 
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "frontend" {
   bucket_prefix = "jenkins-bucket-"
   force_destroy = true
-  
 
   tags = {
     Name = "Jenkins Bucket"
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.frontend.arn}/*"
+      }
+    ]
+  })
 }
